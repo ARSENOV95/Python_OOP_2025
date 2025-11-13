720006bd6030570e097771769558fd46035711e6
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-11-13 14:27:45 +0200

    z

--- LABS/06/06/project/f1_season_app.py
+++ LABS/06/06/project/f1_season_app.py
@@ -2,8 +2,28 @@ from project.formula_teams.red_bull_team import RedBullTeam
 from project.formula_teams.mercedes_team import MercedesTeam
 
 class F1SeasonApp:
-    def __repr__(self):
+    def __init__(self):
         self.red_bull_team: RedBullTeam = None
         self.mercedes_team: MercedesTeam = None
 
+
     def register_team_for_season(self, team_name: str, budget: int):
+        if not team_name in ('Red Bull','Mercedes'):
+            raise ValueError("Invalid team name!")
+
+        if team_name == 'Red Bull':
+            self.red_bull_team = RedBullTeam(budget)
+        else:
+            self.mercedes_team = MercedesTeam(budget)
+
+        return f"{team_name} has joined the new F1 season."
+
+    def new_race_results(self,race_name: str, red_bull_pos: int, mercedes_pos: int):
+        if self.red_bull_team is None or self.mercedes_team is None:
+            raise Exception("Not all teams have registered for the season.")
+
+        rb_revenue = self.red_bull_team.calculate_revenue_after_race(red_bull_pos)
+        mb_revenue = self.mercedes_team.calculate_revenue_after_race(mercedes_pos)
+
+
+        return f"Red Bull: {rb_revenue}. Mercedes: {mb_revenue}. {'Red Bull' if red_bull_pos < mercedes_pos else 'Mercedes'} is ahead at the {race_name} race."
--- LABS/06/06/project/formula_teams/formula_team.py
+++ LABS/06/06/project/formula_teams/formula_team.py
@@ -7,13 +7,49 @@ class FormulaTeam(ABC):
     @property
     def budget(self):
         return self.__budget
-
+    
     @budget.setter
     def budget(self, value):
         if value > 1000000:
             raise ValueError("F1 is an expensive sport, find more sponsors!")
         self.__budget = value
 
+    @property
     @abstractmethod
-    def calculate_revenue_after_race(self,race_pos: int):
+    def RACE_EXPENSES(self) -> int:
         pass
+
+    @property
+    @abstractmethod
+    def title_sponsor(self) -> dict:
+        pass
+
+    @property
+    @abstractmethod
+    def additional_sponsor(self) -> dict:
+        pass
+
+    def calculate_revenue_after_race(self,race_pos: int):
+        revenue = 0
+   
+        if race_pos <= 3:
+            if self.title_sponsor.get(race_pos):
+                revenue += self.title_sponsor[race_pos] 
+            else:
+                revenue += max(self.title_sponsor.values())
+            revenue += + min(self.title_sponsor.values())
+
+        elif race_pos > 3:
+            if self.additional_sponsor.get(race_pos):
+                revenue += self.additional_sponsor[race_pos]
+            
+            elif race_pos < min(self.additional_sponsor.values()):
+                revenue +=  min(self.additional_sponsor.values())
+
+            elif  min(self.additional_sponsor.values()) < race_pos <  max(self.additional_sponsor.values()):
+                revenue += max(self.additional_sponsor.values())
+        revenue -= self.RACE_EXPENSES
+        self.budget += revenue
+        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
+
+            
